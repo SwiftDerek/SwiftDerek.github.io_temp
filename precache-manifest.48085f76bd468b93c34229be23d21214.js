b51f42953f4e840641d6d4484f02d8db2164f821
Derek Williams <souldishwasher@gmail.com>
2020-06-21 19:09:13 -0500

    Updates

---
+++ precache-manifest.48085f76bd468b93c34229be23d21214.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d532ba6df49a927f488da24aab16cd98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95ea5c2ae10eb35bb877",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "3d14c2ca0abce47d207f",
+    "url": "/static/css/main.5f05969b.chunk.css"
+  },
+  {
+    "revision": "95ea5c2ae10eb35bb877",
+    "url": "/static/js/2.448dd0be.chunk.js"
+  },
+  {
+    "revision": "b71cb3cccc73e099d412c24a04bf9105",
+    "url": "/static/js/2.448dd0be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d14c2ca0abce47d207f",
+    "url": "/static/js/main.35273974.chunk.js"
+  },
+  {
+    "revision": "c88671eb88bf6c28dee9",
+    "url": "/static/js/runtime-main.6a060e8a.js"
+  },
+  {
+    "revision": "c2eacd979a8796475193172948a35228",
+    "url": "/static/media/resume.c2eacd97.pdf"
+  }
+]);
